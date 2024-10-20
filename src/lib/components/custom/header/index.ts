9c23a8caef366ee1b42b2bf17055849e9c1352d0
Ingo <me@ingowolf.au>
2024-10-20 12:34:45 +1100

    Modularize navbar

---
+++ src/lib/components/custom/header/index.ts
@@ -0,0 +1,3 @@
+import header from "./header.svelte";
+
+export { header as Header };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
-<script>
+<script lang="ts">
   import "../app.css";
   import { ModeWatcher } from "mode-watcher";
+  import { Header } from "$lib/components/custom/header/index.js";
 </script>
 
 <ModeWatcher />
 <div class="flex min-h-screen w-full flex-col">
+  <Header />
+
   <slot></slot>
 </div>
