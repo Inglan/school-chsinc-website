523b5867fdcc0bdbceddd354115ca2845765ab69
Ingo <me@ingowolf.au>
2024-10-20 11:57:47 +1100

    Add dark toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,10 @@
   import Menu from "lucide-svelte/icons/menu";
   import Package2 from "lucide-svelte/icons/package-2";
   import Search from "lucide-svelte/icons/search";
+  import Sun from "lucide-svelte/icons/sun";
+  import Moon from "lucide-svelte/icons/moon";
 
+  import { toggleMode } from "mode-watcher";
   import { Button } from "$lib/components/ui/button/index.js";
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
   import { Input } from "$lib/components/ui/input/index.js";
@@ -123,6 +126,20 @@
           <DropdownMenu.Item>Logout</DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
+      <Button
+        on:click={toggleMode}
+        variant="secondary"
+        size="icon"
+        class="rounded-full"
+      >
+        <Sun
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Moon
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
     </div>
   </header>
 </div>
