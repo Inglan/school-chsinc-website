3002fe9890d3d7a22637a477cfbbb1747bc26e1e
Ingo <me@ingowolf.au>
2024-10-20 14:13:25 +1100

    Add hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
-Home placeholder
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<div
+  class="relative flex flex-col items-center justify-center bg-cover bg-center text-white text-center"
+  style="background-image: url('https://picsum.photos/1920/1080'); height: calc(100vh - 64px);"
+>
+  <div class="absolute inset-0 bg-black opacity-50"></div>
+  <div class="relative z-10 flex flex-col gap-4">
+    <h1 class="text-5xl">Reliable Home Repairs, Made Easy</h1>
+    <p class="text-xl">
+      Expert quotes and quality repairs delivered to your door
+    </p>
+    <div class="flex"></div>
+  </div>
+</div>
