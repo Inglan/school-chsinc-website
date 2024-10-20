27f1a3595f52887ae994e5d776d98879776eaecb
Ingo <me@ingowolf.au>
2024-10-20 13:24:04 +1100

    Modify toast

--- src/lib/components/custom/header/header.svelte
+++ src/lib/components/custom/header/header.svelte
@@ -122,11 +122,7 @@
                 builders={[builder]}
                 on:click={() => {
                   toast.success("Request submitted", {
-                    description: "Sunday, December 03, 2023 at 9:00 AM",
-                    action: {
-                      label: "Undo",
-                      onClick: () => console.info("Undo"),
-                    },
+                    description: "We will contact you soon",
                   });
                 }}>Submit</Button
               >
