b21b569eb8de4f42dc3d0d2b804ef9f984e50789
Ingo Wolf <me@ingowolf.au>
2024-11-04 23:45:33 +0000

    Add contact pgae

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,14 @@
-contact
+<div class="relative w-full max-w-6xl h-[800px] max-h-screen mx-auto m-5">
+  <!-- Before Image -->
+  <div class="absolute inset-0">
+    <enhanced:img
+      src="$lib/images/photos/contact.jpg"
+      alt="Before Image"
+      class="w-full h-full object-cover brightness-50 rounded-lg"
+    />
+  </div>
+  <!-- Overlay Text -->
+  <div class="absolute inset-0 flex flex-col gap-5 items-center justify-center">
+    <h1 class="text-white text-6xl text-center">Contact Us</h1>
+  </div>
+</div>
