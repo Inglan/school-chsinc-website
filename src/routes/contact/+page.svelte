ba0c31e6ab2b54dd8df3043aecf83bee2e6cfe7e
Ingo Wolf <me@ingowolf.au>
2024-11-07 10:37:40 +0000

    Fix contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+  import { Textarea } from "$lib/components/ui/textarea";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
 <div class="relative w-full max-w-6xl h-[800px] max-h-screen mx-auto mb-5">
   <!-- Before Image -->
   <div class="absolute inset-0">
@@ -11,4 +18,36 @@
   <div class="absolute inset-0 flex flex-col gap-5 items-center justify-center">
     <h1 class="text-white text-6xl text-center">Contact Us</h1>
   </div>
+</div>
+
+<div class="relative w-full max-w-6xl mx-auto mb-5">
+  <form
+    class="space-y-6 max-w-2xl mx-auto"
+    on:submit={function (e) {
+      e.preventDefault();
+      window.open(
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "",
+        "width=400,height=400"
+      );
+    }}
+  >
+    <div class="flex flex-col space-y-1.5">
+      <Label for="name">Name</Label>
+      <Input id="name" type="text" placeholder="Your Name" />
+    </div>
+    <div class="flex flex-col space-y-1.5">
+      <Label for="email">Email</Label>
+      <Input id="email" type="email" placeholder="Your Email" />
+    </div>
+    <div class="flex flex-col space-y-1.5">
+      <Label for="message">Message</Label>
+      <Textarea id="message" placeholder="Your Message" />
+    </div>
+    <div class="text-center">
+      <Button type="submit">Submit</Button>
+    </div>
+  </form>
+  <br />
+  <p class="text-center">Or email us at contact@chsinc.au</p>
 </div>
